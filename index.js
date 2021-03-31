cb3f44e36da0382e9b521681584490c2ac0ed33b
hugur <hugu.r.p+hello@pm.me>
2021-03-31 17:04:50 +0200

    add policy header

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 console.log('privacy-policy pages test')
 
+/* An element for displaying privacy policies */
+class PrivacyPolicy extends HTMLElement {
+    connectedCallback() {
+	this.date = this.getAttribute('date')
+	this.name = this.getAttribute('name')
+	this.render()
+    }
+
+    render() {
+	const $header = document.createElement('privacy-policy-header')
+
+	const $headerTitle = document.createElement('privacy-policy-header-title')
+	$headerTitle.innerText = this.name || 'Privacy Policy'
+
+	const $updatedDate = document.createElement('privacy-policy-updated-date')
+	$updatedDate.innerText = `Updated: ${this.date}`
+
+	this.date && $header.prepend($updatedDate)
+	$header.prepend($headerTitle)
+	this.prepend($header)
+    }
+}
+
+/* An element, to display a section of privacy polcies.
+   - it shows the number of the section
+   - a section should be direct child of a pricacy-policy-list
+ */
 class PrivacyPolicySection extends HTMLElement {
     connectedCallback() {
 	this.number = this.getAttribute('number')
@@ -7,11 +34,12 @@ class PrivacyPolicySection extends HTMLElement {
     }
 
     render() {
+	if (!this.number) return
 	const $number = document.createElement('privacy-policy-section-number')
 	$number.innerText = this.number
 	this.prepend($number)
     }
 }
 
-
+customElements.define('privacy-policy', PrivacyPolicy)
 customElements.define('privacy-policy-section', PrivacyPolicySection)
